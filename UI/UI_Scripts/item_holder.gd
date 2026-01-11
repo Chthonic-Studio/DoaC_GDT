1c5ae28f019ebc20cb940b0409003adc46512e9c
TakaVII <ulirub@gmail.com>
2026-01-11 17:28:52 -0400

    Add initial UI, resources, and scripts for trading system
    Introduces foundational Godot resources (Item, Inventory, NPC, Town), UI scenes for trading, item display, gold counter, notifications, and related scripts. Adds new item and UI assets, updates main UI to include new components, and sets up the world map scene. This establishes the basic structure for a trading and inventory system.

---
+++ UI/UI_Scripts/item_holder.gd
@@ -0,0 +1,15 @@
+extends MarginContainer
+
+@export_category("Background Images")
+@export var unfocused_bg : Texture
+@export var focused_bg : Texture
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
